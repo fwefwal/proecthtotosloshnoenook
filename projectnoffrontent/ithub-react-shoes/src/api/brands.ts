f072f95812be59f06c0d2635e26bb28965f57a25
tukoktprofile <bogdankyzmin11@gmail.com>
2025-11-13 02:49:36 +0300

    Delete projectnoffrontent/ithub-react-shoes directory

--- projectnoffrontent/ithub-react-shoes/src/api/brands.ts
+++
@@ -1,6 +0,0 @@
-import type { Brand } from "../types";
-import fetcher from "./fetcher";
-
-export const brandsApi = {
-    getAll: () => fetcher<Brand[]>("brands")
-}
--- projectnoffrontent/ithub-react-shoes/src/api/categories.ts
+++
@@ -1,6 +0,0 @@
-import type { Category } from "../types";
-import fetcher from "./fetcher";
-
-export const categoriesApi = {
-    getAll: () => fetcher<Category[]>("categories")
-}
